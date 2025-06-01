cf58beb6eb71eaff6131ad9610b6a0ff60dc10d7
Adam Byrne <adam.r.byrne@gmail.com>
2025-06-01 22:18:47 +0100

    fix: increase number of posts/works/projs on / to 4 across the board

--- src/consts.ts
+++ src/consts.ts
@@ -3,9 +3,9 @@ import type { Site, Metadata, Socials } from "@types";
 export const SITE: Site = {
   NAME: "bxrne",
   EMAIL: "adamrbyrne@gmail.com",
-  NUM_POSTS_ON_HOMEPAGE: 3,
-  NUM_WORKS_ON_HOMEPAGE: 2,
-  NUM_PROJECTS_ON_HOMEPAGE: 3,
+  NUM_POSTS_ON_HOMEPAGE: 4,
+  NUM_WORKS_ON_HOMEPAGE: 4,
+  NUM_PROJECTS_ON_HOMEPAGE: 4,
 };
 
 export const HOME: Metadata = {
