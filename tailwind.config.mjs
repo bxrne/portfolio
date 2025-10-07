f4794f6c4ca8c0489d31fe5677852087bfadf929
Adam Byrne <adamrbyrne@gmail.com>
2025-10-07 10:51:59 +0100

    style(gurvbox): better styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,8 +9,85 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["JetBrains Mono", ...defaultTheme.fontFamily.sans],
-        serif: ["JetBrains Mono", ...defaultTheme.fontFamily.serif],
+        sans: ["Geist Mono", ...defaultTheme.fontFamily.sans],
+        serif: ["Geist Mono", ...defaultTheme.fontFamily.serif],
+      },
+      colors: {
+        gruvbox: {
+          bg0_h: {
+            light: "#f9f5d7",
+            dark: "#1d2021",
+          },
+          bg0: {
+            light: "#fbf1c7",
+            dark: "#282828",
+          },
+          bg1: {
+            light: "#ebdbb2",
+            dark: "#3c3836",
+          },
+          bg2: {
+            light: "#d5c4a1",
+            dark: "#504945",
+          },
+          bg3: {
+            light: "#bdae93",
+            dark: "#665c54",
+          },
+          bg4: {
+            light: "#a89984",
+            dark: "#7c6f64",
+          },
+          fg0: {
+            light: "#282828",
+            dark: "#fbf1c7",
+          },
+          fg1: {
+            light: "#3c3836",
+            dark: "#ebdbb2",
+          },
+          fg2: {
+            light: "#504945",
+            dark: "#d5c4a1",
+          },
+          fg3: {
+            light: "#665c54",
+            dark: "#bdae93",
+          },
+          fg4: {
+            light: "#7c6f64",
+            dark: "#a89984",
+          },
+          red: {
+            light: "#9d0006",
+            dark: "#fb4934",
+          },
+          green: {
+            light: "#79740e",
+            dark: "#b8bb26",
+          },
+          yellow: {
+            light: "#b57614",
+            dark: "#fabd2f",
+          },
+          blue: {
+            light: "#076678",
+            dark: "#83a598",
+          },
+          purple: {
+            light: "#8f3f71",
+            dark: "#d3869b",
+          },
+          aqua: {
+            light: "#427b58",
+            dark: "#8ec07c",
+          },
+          orange: {
+            light: "#af3a03",
+            dark: "#fe8019",
+          },
+          gray: "#928374",
+        },
       },
     },
   },
